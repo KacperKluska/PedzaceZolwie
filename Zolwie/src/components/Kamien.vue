1738816acb28374307366ac9ecf4fdc1f69c5786
KacperKluska <kacperkluska130@gmail.com>
2023-10-12 22:39:37 +0200

    feat: wyswietlanie zolwi na kamieniach

---
+++ Zolwie/src/components/Kamien.vue
@@ -0,0 +1,40 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts" setup>
+import Zolw from './Zolw.vue'
+
+const props = defineProps({
+  zolwie: {
+    type: Array,
+    required: true
+  }
+})
+
+console.log('elo', props.zolwie)
+
+const colors = {
+  YELLOW: '#ffff00',
+  RED: '#ff0000',
+  GREEN: '#00ff00',
+  BLUE: '#0000ff',
+  PURPLE: '#800080'
+}
+</script>
+
+<template>
+  <div class="kamien">
+    <Zolw
+      v-for="(zolw, index) in zolwie"
+      :style="{ left: '0px', bottom: `${index * 15}px` }"
+      :color="colors[`${zolw}`]"
+      :key="index"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.kamien {
+  position: absolute;
+  width: 50px;
+  height: 100px;
+}
+</style>
---
+++ Zolwie/src/components/Plansza.vue
@@ -0,0 +1,39 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="js" setup>
+import Kamien from './Kamien.vue'
+import data from '../assets/response.json'
+</script>
+
+<template>
+  <div class="plansza">
+    <h1>Pędzące żółwie</h1>
+    <div class="plansza__container">
+      <img src="../assets/plansza.jpg" alt="plansza gry" />
+      <Kamien style="top: 140px; left: 25px" :zolwie="data.PrzebiegGry[3].Plansza[0].Zolwie" />
+      <Kamien style="top: 70px; left: 60px" />
+      <Kamien style="top: 50px; left: 120px" />
+      <Kamien style="top: 60px; left: 180px" />
+      <Kamien style="top: 120px; left: 225px" />
+      <Kamien style="top: 140px; left: 280px" />
+      <Kamien style="top: 100px; left: 330px" />
+      <Kamien style="top: 40px; left: 380px" />
+      <Kamien style="top: 70px; left: 435px" />
+      <Kamien style="top: 110px; left: 485px" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.plansza {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  &__container {
+    position: relative;
+    width: fit-content;
+  }
+}
+</style>
